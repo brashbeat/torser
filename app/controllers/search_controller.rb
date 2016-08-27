a477a6a86b586389ed56ca0ef2154b3d2a5888aa
logwolvy <amulya.tomer@gmail.com>
2016-08-27 21:16:12 +0000

    mechanised search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,15 +2,29 @@ class SearchController < ApplicationController
 require 'nokogiri'
 require 'open-uri'
 require 'openssl'
+require 'mechanize'
   
   def index
   end
 
   def processor
-    @query = params[:query].strip.downcase.split.join('%20')
-    @address = "https://thepiratebay.org/search/#{@query}/0/99/0" 
-    @base = Nokogiri::HTML(open(@address, :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE))
-    @results = @base.css('.vertTh+ td')
+  if params[:query]
+    # @query = params[:query].strip.downcase.split.join('%20')
+    @query = params[:query].strip.downcase
+    @agent = Mechanize.new
+    @page = @agent.get('https://thepiratebay.org')
+    @form = @page.form('q')
+    @form.q = @query
+    @page = @agent.submit(@form)
+  # @address = "https://thepiratebay.org/search/#{@query}/0/99/0" 
+  # @base = Nokogiri::HTML(open(@address, :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE))
+    @results = @page.css('.vertTh+ td')
+  else
+    redirect_to '/'
+  end  
+  end
+  
+  def next
     
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,12 @@ Rails.application.routes.draw do
   get 'search/index'
 
   post 'search/processor'
+  
+  get 'search/processor'
 
   get 'search/results'
+  
+  get 'search/next'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
