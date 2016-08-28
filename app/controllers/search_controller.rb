b08e7bd315451625835ca3c22d41ad982aa3fb36
logwolvy <amulya.tomer@gmail.com>
2016-08-28 06:48:50 +0000

    tpb functional except seed count

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,25 +3,14 @@ require 'nokogiri'
 require 'open-uri'
 require 'openssl'
 require 'mechanize'
+
   
   def index
   end
 
   def processor
-  if params[:query]
-    # @query = params[:query].strip.downcase.split.join('%20')
-    @query = params[:query].strip.downcase
-    @agent = Mechanize.new
-    @page = @agent.get('https://thepiratebay.org')
-    @form = @page.form('q')
-    @form.q = @query
-    @page = @agent.submit(@form)
-  # @address = "https://thepiratebay.org/search/#{@query}/0/99/0" 
-  # @base = Nokogiri::HTML(open(@address, :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE))
-    @results = @page.css('.vertTh+ td')
-  else
-    redirect_to '/'
-  end  
+    session[:squery] = params[:query].strip.downcase
+    redirect_to '/results/1'
   end
   
   def next
@@ -29,6 +18,45 @@ require 'mechanize'
   end
 
   def results
-    render html: "You searched for #{session[:squery]}"
+    
+    if session[:squery] && params[:pid] == '1'
+      @pid = params[:pid]
+      # @query = params[:query].strip.downcase.split.join('%20')
+      @query = session[:squery]
+    # creating mechanize object
+      @agent = Mechanize.new
+    if session[:agent]
+      @agent.user_agent_alias = session[:agent] 
+    else
+      @uagent = Mechanize::AGENT_ALIASES.keys - ["Mechanize","Windows IE 7","Windows IE 6"]
+      @uagent = @uagent.sample
+      @agent.user_agent_alias , session[:agent] = @uagent , @uagent
+    end  
+      @page = @agent.get('https://thepiratebay.org')
+      @form = @page.form('q')
+      @form.q = @query
+      @page = @agent.submit(@form)
+    # @address = "https://thepiratebay.org/search/#{@query}/0/99/0" 
+    # @base = Nokogiri::HTML(open(@address, :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE))
+      @results = @page.css('.vertTh+ td')
+      @info = @page.css('.vertTh~ td+ td')
+      @link = @page.css('#main-content+ div a')[0]['href']
+      session[:link] = "https://thepiratebay.org" + @link.strip.downcase
+      
+    elsif session[:squery] && ("2".."10").include?(params[:pid])
+      @pid = params[:pid]
+      @query = session[:squery]
+      @agent = Mechanize.new
+      @agent.user_agent_alias = session[:agent]
+      @page = @agent.get(session[:link])
+      @info = @page.css('.vertTh~ td+ td')
+      @link = @page.css('#main-content+ div a')[@pid.to_i]['href']
+      session[:link] = "https://thepiratebay.org" + @link.strip.downcase
+      @results = @page.css('.vertTh+ td')
+    else  
+    redirect_to '/'
+    end
   end
+  
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,10 @@ Rails.application.routes.draw do
 
   post 'search/processor'
   
-  get 'search/processor'
+  get 'search/processor',  to: 'search#index'
 
-  get 'search/results'
+  get 'results/:pid',  to: 'search#results', as: 'page'
   
-  get 'search/next'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
