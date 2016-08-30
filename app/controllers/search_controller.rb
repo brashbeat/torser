306cde7b695acb52b7e8ff8a29d1a2dd2a522170
logwolvy <amulya.tomer@gmail.com>
2016-08-30 03:43:50 +0000

    tpb looks good

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -10,21 +10,31 @@ require 'mechanize'
 
   def processor
     session[:squery] = params[:query].strip.downcase
+    session[:anonymous] = params[:anonymous]
     redirect_to '/results/1'
   end
   
   def next
-    
   end
 
   def results
     
+    ###### assigning user's anonymous setting from session
+    
+    @anon = session[:anonymous]
+    
+    ###### checks if user is searching for the first time(session) or not #######
+    
     if session[:squery] && params[:pid] == '1'
       @pid = params[:pid]
-      # @query = params[:query].strip.downcase.split.join('%20')
       @query = session[:squery]
-    # creating mechanize object
+      
+    ###### creating mechanize object
+    
       @agent = Mechanize.new
+    
+    ###### setting user agent
+    
     if session[:agent]
       @agent.user_agent_alias = session[:agent] 
     else
@@ -32,29 +42,44 @@ require 'mechanize'
       @uagent = @uagent.sample
       @agent.user_agent_alias , session[:agent] = @uagent , @uagent
     end  
+    
+    ###### fetching search page, parsing form and assigning values to form fields
       @page = @agent.get('https://thepiratebay.org')
       @form = @page.form('q')
       @form.q = @query
+    
+    ######  submitting search form and fetching results from returned page   
+      
       @page = @agent.submit(@form)
-    # @address = "https://thepiratebay.org/search/#{@query}/0/99/0" 
-    # @base = Nokogiri::HTML(open(@address, :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE))
       @results = @page.xpath("//table[contains(@id, 'searchResult')]/tr[not(@class='header')]")
+    
+    ###### parsing next page navigation link and saving that to session  
+      
       @link = @page.css('#main-content+ div a')[0]['href']
       session[:link] = "https://thepiratebay.org" + @link.strip.downcase
       
+    ###### checks if user is already in a session and navigating thru links ########
+    
     elsif session[:squery] && ("2".."10").include?(params[:pid])
       @pid = params[:pid]
       @query = session[:squery]
       @agent = Mechanize.new
       @agent.user_agent_alias = session[:agent]
       @page = @agent.get(session[:link])
-      @info = @page.css('.vertTh~ td+ td')
+    
+    ###### parsing next page navigation link and saving that to session  
+      
       @link = @page.css('#main-content+ div a')[@pid.to_i]['href']
       session[:link] = "https://thepiratebay.org" + @link.strip.downcase
+    
+    ######  parsing results from fetched page
       @results = @page.xpath("//table[contains(@id, 'searchResult')]/tr[not(@class='header')]")
+    
+    ###### if no session detected 
     else  
-    redirect_to '/'
+    redirect_to '/' and return
     end
+    render :resultsTPB
   end
   
   
